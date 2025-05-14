aa645993836ddeffee5fcfad7d14b170c0536d63
Ismael Sanz Munoz <isanz-mu@c2r18s6.42madrid.com>
2025-05-14 20:34:37 +0200

    desde 42_3

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isanz-mu < isanz-mu@student.42madrid.co    +#+  +:+       +#+        */
+/*   By: isanz-mu <isanz-mu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 11:34:02 by isanz-mu          #+#    #+#             */
-/*   Updated: 2025/05/14 19:49:57 by isanz-mu         ###   ########.fr       */
+/*   Updated: 2025/05/14 20:22:41 by isanz-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,3 +40,27 @@ int	ft_atoi(const char *str)
 	}
 	return (num * sig);
 }
+/*#include <stdio.h>      // Para printf
+
+
+int main(void)
+{
+    // Probamos con varios ejemplos
+    const char *str1 = "42";
+    const char *str2 = "   -123";
+    const char *str3 = "\t\n  +567";
+    const char *str4 = "abc";
+    const char *str5 = "--42";
+    const char *str6 = "2147483647";     // Máximo valor int
+    const char *str7 = "-2147483648";    // Mínimo valor int
+
+    printf("'%s' -> %d\n", str1, ft_atoi(str1));
+    printf("'%s' -> %d\n", str2, ft_atoi(str2));
+    printf("'%s' -> %d\n", str3, ft_atoi(str3));
+    printf("'%s' -> %d\n", str4, ft_atoi(str4));
+    printf("'%s' -> %d\n", str5, ft_atoi(str5));
+    printf("'%s' -> %d\n", str6, ft_atoi(str6));
+    printf("'%s' -> %d\n", str7, ft_atoi(str7));
+
+    return 0;
+}*/
