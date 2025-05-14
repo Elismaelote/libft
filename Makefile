5a8bebc7f48dc0584a06e93599d520bcb97a4778
Ismael Sanz Munoz <isanz-mu@c2r18s6.42madrid.com>
2025-05-14 19:53:37 +0200

    desde 42_2

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: isanz-mu <isanz-mu@student.42madrid.com    +#+  +:+       +#+         #
+#    By: isanz-mu < isanz-mu@student.42madrid.co    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/08 22:23:29 by isanz-mu          #+#    #+#              #
-#    Updated: 2025/05/09 12:23:07 by isanz-mu         ###   ########.fr        #
+#    Updated: 2025/05/14 19:29:58 by isanz-mu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,12 +22,7 @@ SRC		=	ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 			ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c \
 			ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
 
-BONUS_SRC =	ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c \
-			ft_lstlast_bonus.c ft_lstadd_back_bonus.c ft_lstdelone_bonus.c \
-			ft_lstclear_bonus.c ft_lstiter_bonus.c ft_lstmap_bonus.c
-
 OBJ = $(SRC:.c=.o)
-BOBJ = $(BONUS_SRC:.c=.o)
 
 INCLUDE = libft.h
 
@@ -35,9 +30,6 @@ all: $(NAME)
 
 $(NAME): $(OBJ) 
 	ar rcs $(NAME) $(OBJ)
-
-bonus:
-	@make -s SRC="$(SRC) $(BONUS_SRC)"
 
 %.o: %.c $(INCLUDE)
 	$(CC) $(CFLAGS) -c $< -o $@
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isanz-mu <isanz-mu@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: isanz-mu < isanz-mu@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 22:23:15 by isanz-mu          #+#    #+#             */
-/*   Updated: 2025/05/09 12:23:06 by isanz-mu         ###   ########.fr       */
+/*   Updated: 2025/05/14 19:53:01 by isanz-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,19 +52,4 @@ char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char		*ft_strtrim(char const *s1, char const *set);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
-//PB
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}			t_list;
-void		ft_lstadd_back(t_list **lst, t_list *new);
-void		ft_lstadd_front(t_list **lst, t_list *new);
-void		ft_lstclear(t_list **lst, void (*del)(void *));
-void		ft_lstdelone(t_list *lst, void (*del)(void *));
-void		ft_lstiter(t_list *lst, void (*f)(void *));
-t_list		*ft_lstlast(t_list *lst);
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-t_list		*ft_lstnew(void *content);
-int			ft_lstsize(t_list *lst);
 #endif
